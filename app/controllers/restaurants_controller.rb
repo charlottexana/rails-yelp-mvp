6741d00f38ee18e5d34b7db1391a79b5e52f960d
Charlotte Waller <charlotteawaller@gmail.com>
2020-01-21 21:23:20 +0000

    Completed aside from post commands

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :rating)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+giraffe = Restaurant.new(name: "Giraffe", category: "belgian", phone_number: "01 02 03 04 05", address: "Russell Square")
+giraffe.save!
+
+pizza_express = Restaurant.new(name: "Pizza Express", category: "italian", phone_number: "06 02 03 04 05", address: "Hampstead")
+pizza_express.save!
+
+bunbun = Restaurant.new(name: "Bun Bun", category: "chinese", phone_number: "07 02 03 04 05", address: "Hoxton")
+bunbun.save!
+
+itsu = Restaurant.new(name: "Itsu", category: "japanese", phone_number: "08 02 03 04 05", address: "Farringdon")
+itsu.save!
+
+cote = Restaurant.new(name: "Cote", category: "french", phone_number: "09 02 03 04 05", address: "Southbank")
+cote.save!
